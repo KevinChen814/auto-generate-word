71000505eedc5c3c3d8f978d823a745ed81767b6
Kevin Chen <96588995+KevinChen814@users.noreply.github.com>
2023-11-28 11:17:22 +0800

    modify_mainFunction

--- guarantee.py
+++ guarantee.py
@@ -20,7 +20,7 @@ def guarantee_content(date, customer_name, customer_id, amount):
     # Document content
     doc.add_heading('EVOX(易喂)服務保證金繳交證明', level=1)
     doc.add_paragraph('\n日期:  中華民國  '+ date[0:3] +'  年  '+ date[3:5] +' 月 '+ date[5:] +' 日 ')
-    doc.add_paragraph('客戶名稱 : '+ customer_name + ' (統編: '+ customer_id +') ')
+    doc.add_paragraph('客戶名稱 : '+ customer_name + ' ( 統編: '+ customer_id +' ) ')
     doc.add_paragraph('茲收到客戶繳交之EVOX(易喂)服務保證金共計新台幣____' + amount  +'____元，關於此保證金之歸還，請詳見客戶與本公司簽署之EVOX(易喂)服務申請書附件之EVOX服務契約條款第二條第7、8項。 ')
     doc.add_paragraph('易得雲端股份有限公司 \n統編: 90616421 \n地址: 台北市114內湖區內湖路一段316號5樓之1 ')
 
@@ -28,9 +28,10 @@ def guarantee_content(date, customer_name, customer_id, amount):
     for paragraph in doc.paragraphs:
         paragraph.style.font.size = Pt(12)
 
+    # adjust the heading
     for paragraph in doc.paragraphs:
         if paragraph.style.name.startswith('Heading'):
-            paragraph.paragraph_format.alignment=WD_ALIGN_PARAGRAPH.CENTER # LEFT, CENTER, RIGHT
+            paragraph.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER # LEFT, CENTER, RIGHT
             for run in paragraph.runs:
                 run.font.color.rgb = RGBColor(0, 0, 0)
                 run.font.name = '標楷體'
@@ -44,9 +45,9 @@ def guarantee_content(date, customer_name, customer_id, amount):
 
     doc.save(word_file_path) # 儲存word檔
 
-    return word_file_path
+    transfer_word_to_pdf(folder_path,customer_name,word_file_path)
 
-def transfer_word_to_pdf(word_file_path):
+def transfer_word_to_pdf(folder_path,customer_name,word_file_path):
 
     # transfer word file to pdf
     pdf_file_path = os.path.join(folder_path, customer_name +'.pdf') #指定PDF儲存路徑
@@ -68,7 +69,4 @@ while True:
             if len(customer_id) == 8:
                 break
     amount = input('保證金金額: ')
-
-    word_file_path = guarantee_content(date, customer_name, customer_id, amount)
-
-    transfer_word_to_pdf(word_file_path)
+    guarantee_content(date, customer_name, customer_id, amount)
